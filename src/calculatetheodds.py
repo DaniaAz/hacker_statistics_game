92e420582ff3f51485031ed5f77594a35191c8b9
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-03-24 20:49:21 +0100

    prepare to simulate multiple random walks
    having a single random walk does not say much about our wining
    chances.
    To get an idea about how big our chances are of reaching the
    final step, we should simulate more random walks and collect the results.

--- src/calculatetheodds.py
+++ src/calculatetheodds.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-random_walk = [0]
+all_walks = []
 
-for x in range(100) :
-    step = random_walk[-1]
-    dice = np.random.randint(1,7)
+for i in range(10):
+    random_walk = [0]
 
-    if dice <= 2:
-        step = max(0 , step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1,7)
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
 
-    random_walk.append(step)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    all_walks.append(random_walk)
 
 plt.plot(random_walk)
 plt.show()
