4ba501fd46040f0ea7c0154447e0643fd31baa9a
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-03-24 20:39:56 +0100

    prepare to simulate for a random walk
    The for loop should help calculate our location at the top of the
    tower after throwing the dice for 100 times

--- src/calculatetheodds.py
+++ src/calculatetheodds.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-step = 50
+random_walk = [0]
 
-dice = np.random.randint(1,7)
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
 
-if dice <= 2 :
-    step = step - 1
-elif dice == 3 or dice == 4 or dice == 5  :
-    step+= 1
-else :
-    step = step + np.random.randint(1,7)
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+        
+    random_walk.append(step)
