5c3ecd914e94e740cc60c9d4b808e918b98fc3b8
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-03-24 20:36:58 +0100

    determine next step assuming we are at step 50

--- src/calculatetheodds.py
+++ src/calculatetheodds.py
@@ -1,3 +1,12 @@
 import numpy as np
 
+step = 50
+
 dice = np.random.randint(1,7)
+
+if dice <= 2 :
+    step = step - 1
+elif dice == 3 or dice == 4 or dice == 5  :
+    step+= 1
+else :
+    step = step + np.random.randint(1,7)
