7c0e5d9d5147a0f4ec9ac20f9debe644d354af87
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-03-24 20:45:07 +0100

    use max to make sure step can not go below 0

--- src/calculatetheodds.py
+++ src/calculatetheodds.py
@@ -7,10 +7,10 @@ for x in range(100) :
     dice = np.random.randint(1,7)
 
     if dice <= 2:
-        step = step - 1
+        step = max(0 , step - 1)
     elif dice <= 5:
         step = step + 1
     else:
         step = step + np.random.randint(1,7)
-        
+
     random_walk.append(step)
